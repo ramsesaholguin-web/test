54ae4b4f043b851426283a10bf9dedd9649cc8c7
Gael Ronquillo <gael.ronquillo@outlook.com>
2025-10-14 16:52:05 -0600

    Add Warnings widget to display statistics for total, active, and resolved warnings

---
+++ app/Filament/Resources/UserStatuses/Widgets/UserStatuses.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\UserStatuses\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class UserStatuses extends StatsOverviewWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            //
+        ];
+    }
+}
