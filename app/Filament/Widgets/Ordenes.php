7b07528ef09b44f3bc18b804232a4c26cca5f044
Gael Ronquillo <gael.ronquillo@outlook.com>
2025-10-20 15:19:02 -0600

    Add Ordenes widget skeleton for order statistics

---
+++ app/Filament/Widgets/Ordenes.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+
+class Ordenes extends ChartWidget
+{
+    protected ?string $heading = 'Ordenes';
+
+    protected function getData(): array
+    {
+        return [
+            Sats::make('Ordenes por Mes', [
+                'Enero' => 30,
+                'Febrero' => 45,
+                'Marzo' => 25,
+                'Abril' => 60,
+                'Mayo' => 50,
+                'Junio' => 70,
+            ])->color('blue'),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
