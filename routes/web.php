f97a8275f05ebff01d59bf64e22e0cc7dc9cd8c1
Ramses-Holguin-Web <ramsesaholguin@gmail.com>
2025-12-02 15:56:45 -0700

    Redirect root URL to Filament admin for streamlined access.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Redirect the root URL directly to the Filament admin
+Route::redirect('/', '/admin');
